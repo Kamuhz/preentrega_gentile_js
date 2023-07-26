892e835d7b1e0d505852e06d3ba2e8cc83b364b3
Ivan Gentile <ivangentile91@gmail.com>
2023-07-26 10:55:07 -0300

    juego carrera

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
-//----------- Pre-Entrega1 Gentile -----------//
-
-const stamina = () => Math.ceil(Math.random()*7)
+const stamina = () => Math.ceil(Math.random() * 10);
 let staminaJorge = stamina();
 let staminaIvan = stamina();
 let vueltas = 0;
 let fatiga = 1;
 
-
-while(staminaJorge>0 && staminaIvan>0){
-vueltas +=1;
-staminaJorge -= fatiga;
-staminaIvan -= fatiga;
-console.log('---- N° De Vueltas ' + vueltas + ' ----');
-console.log('El Aguante de jorge es de ' + staminaJorge);
-console.log('El Aguante de Ivan es de ' + staminaIvan);
+function mostrarProgreso(vueltas, aguanteJorge, aguanteIvan) {
+    console.log(`---- N° De Vueltas ${vueltas} ----`);
+    console.log(`El Aguante de Jorge es de ${aguanteJorge}`);
+    console.log(`El Aguante de Ivan es de ${aguanteIvan}`);
 }
 
-if(staminaJorge>staminaIvan) {
-console.log('Ivan no puede Continuar');
-console.log('------------------------')
-console.log('Jorge Consigue la victoria!!');
-
-}else if (staminaJorge == staminaIvan){
-console.log('Es un Empate')
-}else {
-console.log('Jorge no puede Continuar');
-console.log('------------------------')   
-console.log('Ivan Consigue la victoria!!');
+function determinarGanador(aguanteJorge, aguanteIvan) {
+    if (aguanteJorge > aguanteIvan) {
+        console.log('Ivan no puede Continuar');
+        console.log('------------------------');
+        console.log('Jorge Consigue la victoria!!');
+} else if (aguanteJorge === aguanteIvan) {
+    console.log('Es un Empate');
+} else {
+    console.log('Jorge no puede Continuar');
+    console.log('------------------------');
+    console.log('Ivan Consigue la victoria!!');
+}
 }
 
+function reiniciarCarrera() {
+    staminaJorge = stamina();
+    staminaIvan = stamina();
+    vueltas = 0;
+    console.log('¡Carrera reiniciada!');
+    comenzarCarrera();
+}
+function comenzarCarrera() {
+    if (staminaJorge > 0 && staminaIvan > 0) {
+        vueltas++;
+        staminaJorge -= fatiga;
+        staminaIvan -= fatiga;
+        mostrarProgreso(vueltas, staminaJorge, staminaIvan);
 
+    // Delay de 1 segundos antes de la siguiente vuelta
+    setTimeout(comenzarCarrera, 1000);
+} else {
+    determinarGanador(staminaJorge, staminaIvan);
+}
+}
 
+comenzarCarrera();
